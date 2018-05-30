1b436ab7065c99fbe2b5c5e14e0c1453375b65b8
HodgesWardElliott <bbachrach@hodgeswardelliott.com>
2018-05-30 14:49:00 -0400

    Minor changes

--- pediacities_join.R
+++ pediacities_join.R
@@ -7,8 +7,23 @@ require(sf)
 require(dplyr)
 options(scipen=999)
 
-# Read in pediacities shapefile -------------------------------------------
+pluto <- readRDS("/Users/billbachrach/Dropbox (hodgeswardelliott)/Data Science/Project Data/PLUTO/pluto_lean_compressed_2003_2017.rds") %>%
+  filter(Year >= 2009) %>%
+  arrange(desc(Year)) %>%
+  filter(!is.na(lat)) %>%
+  filter(!duplicated(BBL)) %>%
+  mutate(
+    BBL = paste(
+      str_sub(BBL,start=1,end=1)
+      ,as.numeric(Block)
+      ,as.numeric(Lot)
+      ,sep="_"
+    )
+  ) %>%
+  select(BBL,lat,lon)
+  
 
+# Read in pediacities shapefile -------------------------------------------
 
 pediashape.url <- "http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson"
 
@@ -38,3 +53,6 @@ pluto <- left_join(pluto
                    ,by="BBL"
 ) %>%
   select(-geometry)
+
+
+saveRDS(pluto,"/Users/billbachrach/Dropbox (hodgeswardelliott)/Data Science/Project Data/PLUTO/pluto_neighborhoods.rds")
